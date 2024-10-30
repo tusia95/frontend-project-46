72f31c66a38e4be4cb294723dbac6793355ca5cf
Alina Tabachnikas <alina.tabachnikas@skyeng.ru>
2024-10-30 14:32:00 +0200

    add files from CLI json parsing

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { getAbsoluteFilePath, getFileExtension, readFile } from "../src/helpers/files.js";
 const program = new Command();
 
 
 program.option('-v, --version', 'output the version number')
   .argument('<filepath1>', 'path to first file')
   .argument('<filepath2>', 'path tp second file')
-  .option('-f, --format', 'output format' )
-  .description('Compares two configuration files and shows a difference.');
+  .option('-f, --format [type]', 'output format' )
+  .description('Compares two configuration files and shows a difference.')
+  .action((filepath1, filepath2, options,) => {
+   // const filePath = 'src/data/file1.json'
+    const absolutePath1 = getAbsoluteFilePath(filepath1);
+    console.log(JSON.parse(readFile(absolutePath1)));
+
+    const absolutePath2 = getAbsoluteFilePath(filepath2);
+      console.log(JSON.parse(readFile(absolutePath2)));
+});
 
 program.parse(process.argv);
 
---
+++ src/helpers/files.js
@@ -0,0 +1,33 @@
+import { cwd } from 'node:process';
+import path from 'node:path';
+import fs from 'node:fs';
+
+
+
+const getAbsoluteFilePath = (relativePath) => {
+  return path.resolve(getCurrentWorkingDirectory(), relativePath);
+}
+
+const getCurrentWorkingDirectory = () => {
+  return cwd();
+}
+
+const readFile = (absolutePath) => {
+  if(fs.existsSync(absolutePath)) {
+    return fs.readFileSync(absolutePath, 'utf8');
+  } else {
+    throw new Error(`File with path ${absolutePath} doesn't exist`)
+  }
+}
+
+function getFileExtension(filePath) {
+  // Ensure the file exists before trying to get the extension
+  if (fs.existsSync(filePath)) {
+    return path.extname(filePath);
+  } else {
+    console.log('File does not exist');
+    return null;
+  }
+}
+
+export { getAbsoluteFilePath, readFile, getFileExtension };
